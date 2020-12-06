9b3a4d4e664af1401a73f14b613a24c61c4e3a89
Alex Shortt <alexander.shortt@gmail.com>
2020-12-05 17:35:13 -0800

    remove everything but the rocks

--- src/components/SpacesHome/index.tsx
+++ src/components/SpacesHome/index.tsx
@@ -1,8 +1,4 @@
-import AboutSpace from "models/AboutSpace";
-import ContactSpace from "models/ContactSpace";
-import ServiceSpace from "models/ServiceSpace";
 import Rocks from "models/Rocks";
-import Buildings from "models/Buildings";
 import { Logo } from "spacesvr";
 import Orbiting from "../../modifiers/Orbiting";
 
@@ -10,17 +6,7 @@ const SpacesHome = (props: JSX.IntrinsicElements["group"]) => {
   return (
     <group {...props}>
       <group scale={[0.00275, 0.00275, 0.00275]}>
-        {/*<group position={[17, 0, 3.5]} rotation-y={-0.15}>*/}
-        {/*  <AboutSpace />*/}
-        {/*</group>*/}
-        {/*<group position={[-16, 0, -3]} rotation-y={Math.PI / 2 + 0.3}>*/}
-        {/*  <ContactSpace />*/}
-        {/*</group>*/}
-        {/*<group position-z={18} rotation-y={-Math.PI / 2}>*/}
-        {/*  <ServiceSpace />*/}
-        {/*</group>*/}
         <Rocks />
-        <Buildings />
         <Orbiting dist={9}>
           <Logo
             floating
